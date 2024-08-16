f5f11314e520fcb0f1d35eed4828c86f9fcead51
kartik <kartikmendu@gmail.com>
2024-08-16 17:38:09 +0530

    Made changes in prisma to make it work on edge.

--- lib/DBClient.ts
+++ lib/DBClient.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
 
-const singletonClient = () => new PrismaClient();
+const singletonClient = () => {
+  const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaNeon(neon);
+  const prisma = new PrismaClient({ adapter });
+  return prisma;
+};
 
 declare global {
   var prismaGlobal: undefined | ReturnType<typeof singletonClient>;
